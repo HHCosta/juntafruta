c7df568d34082117cf4fd940f05131e2d244e07f
Heitor Hayato <heitorhayato@gmail.com>
2024-07-05 21:08:51 -0300

    localização e formulário de coleta

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,8 @@
+var userLat = null;
+var userLng = null;
+var mapa = null;
+var pinIcon = null;
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -5,9 +10,11 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 
+    
     startAppMenu();
     startModalColeta();
     startMapa();
+    starGeolocation();
 }
 
 
@@ -19,8 +26,12 @@ function startAppMenu()
 function startModalColeta()
 {
     $('#modalColeta').modal();
-    $('#btnColeta').on('click', function() {
+    $('#btnColeta').on('click', async function() {
         $('#modalColeta').modal('open');
+        var endColeta = await getAddressFromLatLng(userLat, userLng);
+        // var valorEndereco = `${endColeta.address.city}, ${endColeta.address.suburb}, ${endColeta.address.road}, ${endColeta.address.house_number}, ${endColeta.address.postcode}`;
+        var valorEndereco = `${endColeta.address.road}, ${endColeta.address.house_number} - ${endColeta.address.suburb} - ${endColeta.address.city} - ${endColeta.address.state} - ${endColeta.address.postcode}`;
+        $("#txtEnderecoColeta").val(valorEndereco);
     });
     
     $('#btnModalColetaCancelar').on('click', function(){
@@ -36,10 +47,50 @@ function startMapa()
             position:"topleft"
         }
     }
-    var mapa=L.map("mapa",opcoes);
+
+    mapa=L.map("mapa",opcoes);
     mapa.setView([-23.572683, -46.625513], 18);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         maxZoom: 19
     }).addTo(mapa);
+
+    pinIcon = L.icon({
+        iconUrl: 'img/pin.png',
+        iconSize: [32, 32]
+    });
 }
+
+function starGeolocation()
+{
+    navigator.geolocation.getCurrentPosition(geolocationResponse, geolocationError);
+}
+
+function geolocationResponse(position)
+{
+    userLat = position.coords.latitude;
+    userLng = position.coords.longitude;
+    mapa.setView([userLat, userLng]);
+
+    L.marker([userLat, userLng], {icon: pinIcon}).addTo(mapa);
+}
+
+function geolocationError(error)
+{
+    alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
+}
+
+function getAddressFromLatLng(lat, lng)
+{
+    return new Promise((resolve, reject) => {
+        $.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`, function(data){
+            // console.log(data.address.road);
+            resolve(data);
+    
+        });
+    })
+
+
+}
+
