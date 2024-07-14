4f8abb6c9f8030ef1df99bf0c01eca62fc86a7f0
Heitor Hayato <heitorhayato@gmail.com>
2024-07-14 01:05:43 -0300

    salvar no servidor

--- server/server.js
+++ server/server.js
@@ -1,16 +1,67 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const cors = require("cors")
-const app = express();
-const port = 3000;
+const express       = require('express');
+const bodyParser    = require("body-parser");
+const cors          = require("cors");
+const mysql         = require('mysql');
+const dbOptions     = require('./db.json');
+
+const app       = express();
+const port      = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+const con = mysql.createConnection(dbOptions);
+
 app.post('/savecoleta', (req, res) => {
     const coleta = req.body;
     console.log(coleta);
-    res.send('OK');
+
+    con.connect(function(err) {
+        if (err)
+        {
+            console.log(err);
+
+            res.status(500).send({
+                "status": err
+            });
+
+            con.end();
+            return;
+        }
+
+        console.log("Connected!");
+        
+        const sqlInsert = `
+            INSERT INTO coletas(device_id, endereco, peso, itens_coleta, nome, phone)
+            VALUES(?, ?, ?, ?, ?, ?)
+        `;
+
+        const sqlInsertValues = [coleta.deviceid, coleta.endereco, coleta.peso, coleta.itens_coleta, coleta.nome, coleta.phone];
+
+        con.query(sqlInsert, sqlInsertValues, function (err, result) {
+            if (err)
+            {
+                console.log(err);
+
+                res.status(500).send({
+                    "status": err
+                });
+
+                con.end();
+                return;
+            }
+
+            // console.log("Result: " + result);
+
+            res.status(200).send({
+                "status": "OK"
+            });
+
+            con.end();
+        });
+    });
+
+
 })
 
 app.listen(port, () => {
